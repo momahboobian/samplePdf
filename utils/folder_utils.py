16d09a8627af6c66bbb5c6d7cef95ecd97b59808
Mo Mahboobian <momahboobian@gmail.com>
2024-08-14 23:40:23 +0100

    Updating check Folder upload empty

--- utils/folder_utils.py
+++ utils/folder_utils.py
@@ -4,12 +4,21 @@ import shutil
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 
 def is_upload_folder_empty():
-    return not os.listdir(UPLOAD_FOLDER)
+    if not os.path.exists(UPLOAD_FOLDER):
+        return True
+    return len(os.listdir(UPLOAD_FOLDER)) == 0
+
+
 
 def empty_upload_folder():
-    for filename in os.listdir(UPLOAD_FOLDER):
-        file_path = os.path.join(UPLOAD_FOLDER, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
+    if os.path.exists(UPLOAD_FOLDER):
+        shutil.rmtree(UPLOAD_FOLDER)
+
+        for root, dirs, files in os.walk(UPLOAD_FOLDER, topdown=False):
+            for file in files:
+                os.remove(os.path.join(root, file))
+            for dir in dirs:
+                os.rmdir(os.path.join(root, dir))
+        os.rmdir(UPLOAD_FOLDER)
+    os.makedirs(UPLOAD_FOLDER)
+    return {"message": "Folder cleared successfully."}
