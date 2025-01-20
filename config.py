1bb8798108b4c71d1642176e93c46b0f6d4a8560
Mo Mahboobian <momahboobian@gmail.com>
2025-01-20 17:48:30 +0000

    feat: Database configuration

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
-LOG_LEVEL = 'DEBUG'
-
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+LOG_LEVEL = 'DEBUG'
+
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
