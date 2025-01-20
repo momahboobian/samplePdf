a4c9a65049c776beefc917185ba9030f0b4e4f50
Mo Mahboobian <momahboobian@gmail.com>
2025-01-20 18:14:18 +0000

    feat: Adding a check to prevent duplicate transaction_id

--- db/populate_data.py
+++ db/populate_data.py
@@ -10,11 +10,12 @@ def populate_data(batch_id, invoice_data, site_data, grand_totals):
     cursor = connection.cursor()
 
     try:
-        # Insert invoices data
+        # Insert invoices data with check for duplicates
         for invoice in invoice_data:
             cursor.execute("""
                 INSERT INTO invoices (batch_id, filename, payment_date, transaction_id, total)
                 VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (transaction_id) DO NOTHING  -- Prevent duplicate transaction_id
             """, (batch_id, invoice['filename'], invoice['payment_date'], invoice['transaction_id'], invoice['total']))
 
         # Insert site totals data
@@ -22,6 +23,7 @@ def populate_data(batch_id, invoice_data, site_data, grand_totals):
             cursor.execute("""
                 INSERT INTO site_totals (batch_id, invoice_id, site_name, total)
                 VALUES (%s, %s, %s, %s)
+                ON CONFLICT (batch_id, invoice_id, site_name) DO NOTHING  -- Prevent duplicate site_totals
             """, (batch_id, site['invoice_id'], site['site_name'], site['total']))
 
         # Insert grand totals data
@@ -29,6 +31,7 @@ def populate_data(batch_id, invoice_data, site_data, grand_totals):
             cursor.execute("""
                 INSERT INTO grand_totals (batch_id, site_name, total, total_of_grand_totals)
                 VALUES (%s, %s, %s, %s)
+                ON CONFLICT (batch_id, site_name) DO NOTHING  -- Prevent duplicate grand_totals
             """, (batch_id, grand_total['site_name'], grand_total['total'], grand_total['total_of_grand_totals']))
 
         connection.commit()
