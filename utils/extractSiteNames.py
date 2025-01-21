ed0ca8f6efdd250569178ad7b9a39d94efc0aca2
Mo Mahboobian <momahboobian@gmail.com>
2025-01-21 17:33:19 +0000

    feat: Update the extraction process function to handle better extracting

--- utils/extractSiteNames.py
+++ utils/extractSiteNames.py
@@ -1,8 +1,10 @@
 import re
+from decimal import Decimal, ROUND_HALF_UP
 
 site_names = [
     "Aldgate",
     "Birmingham",
+    "Brunch - 2024 – Conversion",
     "Bournemouth",
     "Cambridge",
     "Canterbury",
@@ -15,6 +17,7 @@ site_names = [
     "Ipswich",
     "Lakeside",
     "Leeds",
+    "Leads",
     "Liverpool",
     "Manchester",
     "Norwich",
@@ -31,17 +34,28 @@ site_names = [
 ]
 
 
-def extract_site_names(text, site_names):
-    matched_site_names = []
+def extract_site_names(text):
+    matched_site_names = {}
+    total_amount = Decimal(0)
     lines = text.split('\n')
     for i, line in enumerate(lines):
-        if "From" in line:
-            for j in range(i-1, -1, -1):
-                if any(keyword.lower() in lines[j].lower() for keyword in ["boom", "post"]):
-                    for site in site_names:
-                        if site.lower() in lines[j].lower():
-                            amount = re.search(r'£(\d+\.\d{2})', lines[i]).group(1)
-                            matched_site_names.append((site, amount))
-                            break
-                    break
+        if "Boom_Facebook" in line:
+            site_name = next((site for site in site_names if site.lower() in line.lower()), None)
+            if site_name:
+                for j in range(i+1, len(lines)):
+                    if "AM" in lines[j] or "PM" in lines[j]:
+                        try:
+                            amount = re.search(r'(\d+\.\d{2})', lines[j]).group(1)
+                            amount = Decimal(amount).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+                            matched_site_names[site_name] = matched_site_names.get(site_name, Decimal(0)) + amount
+                            total_amount += amount
+                        except AttributeError:
+                            print(f"Error: Could not find amount in line '{lines[j]}'")
+                        break
+    # Add amounts for "Leads" to "Leeds"
+    if "Leads" in matched_site_names:
+        matched_site_names["Leeds"] = matched_site_names.get("Leeds", Decimal(0)) + matched_site_names["Leads"]
+        del matched_site_names["Leads"]
+    print(matched_site_names)
+    print(f"Total Amount: {total_amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)}")
     return matched_site_names
