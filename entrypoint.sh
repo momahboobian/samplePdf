9caed515553a49927f4ce454251e4b2a29f58567
Mo Mahboobian <momahboobian@gmail.com>
2025-01-21 20:06:25 +0000

    Feat: Updating entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
 
-# python -m flask run --host=0.0.0.0
+python -m flask run --host=0.0.0.0
 # gunicorn -w 4 -b 0.0.0.0:$PORT app:app
 # gunicorn -w 4 -b 0.0.0.0:5001 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app
-uwsgi --http :5000 --wsgi-file app.py --callable app
+# uwsgi --http :5000 --wsgi-file app.py --callable app
 
