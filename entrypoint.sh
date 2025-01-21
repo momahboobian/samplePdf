2c1785c1c5628c37c27d0afe07e3fd05a9508ae0
Mo Mahboobian <momahboobian@gmail.com>
2025-01-21 20:03:13 +0000

    feat: update to use uwsgi for socket connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -ex
 
 # python -m flask run --host=0.0.0.0
 # gunicorn -w 4 -b 0.0.0.0:$PORT app:app
-gunicorn -w 4 -b 0.0.0.0:5001 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app
-# uwsgi --http :5000 --wsgi-file app.py --callable app
+# gunicorn -w 4 -b 0.0.0.0:5001 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app
+uwsgi --http :5000 --wsgi-file app.py --callable app
 
