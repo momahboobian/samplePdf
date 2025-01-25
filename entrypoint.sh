f1a69a8758a2d1dbfe77aba36234f70ddecb0935
Mo Mahboobian <moreelltd@gmail.com>
2025-01-25 17:09:45 +0000

    feat: Updating bind port back

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -ex
 
 # python -m flask run --host=0.0.0.0
-gunicorn --bind 0.0.0.0:5001 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app
+gunicorn --bind 0.0.0.0:5000 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app
 
